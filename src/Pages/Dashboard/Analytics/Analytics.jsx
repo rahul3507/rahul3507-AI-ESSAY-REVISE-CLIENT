92183bc7b3180a6ce1e19bbd30b70447b092d02b
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-08-03 03:25:01 -0600

    essay radar card created using rechart

--- src/Pages/Dashboard/Analytics/Analytics.jsx
+++ src/Pages/Dashboard/Analytics/Analytics.jsx
@@ -2,8 +2,10 @@
 
 const Analytics = () => {
   return (
-    <div>
-      <h1>Analytics</h1>
+    <div className="px-4">
+      <h1 className="text-2xl md:text-3xl font-bold text-black">Analytics</h1>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mr-4"></div>
     </div>
   );
 };
