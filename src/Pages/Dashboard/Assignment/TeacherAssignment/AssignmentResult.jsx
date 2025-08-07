21365dd63a66fef6a06ab86467fc2a7d65f40142
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-08-06 23:56:59 -0600

    install shadcn ui and added into assignmentresult

--- src/Pages/Dashboard/Assignment/TeacherAssignment/AssignmentResult.jsx
+++ src/Pages/Dashboard/Assignment/TeacherAssignment/AssignmentResult.jsx
@@ -3,16 +3,21 @@
 "use client";
 
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Checkbox } from "@/components/ui/checkbox";
+import { Button } from "../../../../components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "../../../../components/ui/card";
+import { Checkbox } from "../../../../components/ui/checkbox";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select";
+} from "../../../../components/ui/select";
 
 import { FileText } from "lucide-react";
 
