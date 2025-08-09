4a66da7d8046ae399eff4ad82bc2c3467c2c33b4
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-08-09 00:12:17 -0600

    Essay page completed

--- src/Pages/Dashboard/Essays/Essays.jsx
+++ src/Pages/Dashboard/Essays/Essays.jsx
@@ -1,9 +1,19 @@
 /** @format */
 
+import TeachersEssayTable from "../HomePage/TeacherHome/TeachersEssayTable";
+
 const Essays = () => {
   return (
-    <div>
-      <h1>Essays</h1>
+    <div className="px-4">
+      {/* Header */}
+      <div className="flex items-center justify-between mb-8">
+        <div>
+          <h1 className="text-3xl font-bold text-black mb-2">Essays</h1>
+        </div>
+      </div>
+      <div>
+        <TeachersEssayTable />
+      </div>
     </div>
   );
 };
