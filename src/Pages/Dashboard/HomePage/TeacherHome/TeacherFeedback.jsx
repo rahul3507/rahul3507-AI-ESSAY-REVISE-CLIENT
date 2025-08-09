ac96e0e69cb3e142423c25ea144bb48c5d6de631
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-08-08 23:48:03 -0600

    set selected border

--- src/Pages/Dashboard/HomePage/TeacherHome/TeacherFeedback.jsx
+++ src/Pages/Dashboard/HomePage/TeacherHome/TeacherFeedback.jsx
@@ -125,10 +125,10 @@ export default function TeacherFeedback() {
                   value={selectedGrammar}
                   onValueChange={setSelectedGrammar}
                 >
-                  <SelectTrigger className="w-full cursor-pointer">
+                  <SelectTrigger className="w-full cursor-pointer border border-gray-200">
                     <SelectValue placeholder="Select score" />
                   </SelectTrigger>
-                  <SelectContent className="bg-white cursor-pointer">
+                  <SelectContent className="bg-white cursor-pointer border-gray-200">
                     {Array.from({ length: 25 }, (_, i) => (
                       <SelectItem
                         key={i + 1}
