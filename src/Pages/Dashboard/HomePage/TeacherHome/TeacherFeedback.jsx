ac2b453a0c180d7c68b2c9d8e6e002ee55116b70
RAHUL MALLIK <rmallik191242@bscse.uiu.ac.bd>
2025-08-08 23:49:44 -0600

    set border for select

--- src/Pages/Dashboard/HomePage/TeacherHome/TeacherFeedback.jsx
+++ src/Pages/Dashboard/HomePage/TeacherHome/TeacherFeedback.jsx
@@ -187,10 +187,10 @@ export default function TeacherFeedback() {
                   value={selectedClarity}
                   onValueChange={setSelectedClarity}
                 >
-                  <SelectTrigger className="w-full cursor-pointer">
+                  <SelectTrigger className="w-full cursor-pointer border-gray-200">
                     <SelectValue placeholder="Select score" />
                   </SelectTrigger>
-                  <SelectContent className="bg-white cursor-pointer">
+                  <SelectContent className="bg-white cursor-pointer border-gray-200">
                     {Array.from({ length: 25 }, (_, i) => (
                       <SelectItem
                         key={i + 1}
